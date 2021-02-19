0295d14960cd5eb030e111317b386d74bc974ae0
dn070017 <dn070017@gmail.com>
2021-02-19 01:04:02 +0100

    bug fixed for qsmooth_normalization return with quantile normalized data

--- scripts/qsmooth_normalization.R
+++ scripts/qsmooth_normalization.R
@@ -6,13 +6,22 @@ count <- args[1]
 group <- args[2]
 
 prefix <- substring(count, 1, nchar(count) - 9)
-out_file_name <- paste0(prefix, 'qsmooth.tsv')
+out_file_name_qsmooth <- paste0(prefix, 'qsmooth.tsv')
+out_file_name_quantile <- paste0(prefix, 'quantile.tsv')
 
 count <- read.table(count, sep='\t', header=TRUE, stringsAsFactors=FALSE, row.names=1, check.names=FALSE)
 group  <- read.table(group, sep='\t', header=FALSE, stringsAsFactors=FALSE)
 
 qsmooth_model <- qsmooth(count, group[, 2])
 norm <- qsmoothData(qsmooth_model)
+rownames(norm) <- rownames(count)
+colnames(norm) <- colnames(count)
+
+write.table(
+    norm, 
+    out_file_name_qsmooth,
+    sep='\t', quote=FALSE, row.names=TRUE, col.names=NA
+)
 
 norm <- normalize.quantiles(as.matrix(count))
 rownames(norm) <- rownames(count)
@@ -20,6 +29,6 @@ colnames(norm) <- colnames(count)
 
 write.table(
     norm, 
-    out_file_name,
+    out_file_name_quantile,
     sep='\t', quote=FALSE, row.names=TRUE, col.names=NA
 )
