22e4092f769721ec5dff48eaa901ddc18531d22f
dn070017 <dn070017@gmail.com>
2022-04-12 13:53:25 +0200

    modify analysis; adjust the figure size; fix bugs for label color

---
+++ scripts/comparison.py
@@ -0,0 +1,89 @@
+#%%
+import numpy as np
+import pandas as pd
+import itertools as it
+import os
+import sys
+import yaml
+from utils import compute_correlation_coefficients, extract_tissue_exclusive_genes, sort_xprs_samples, bokeh_area_under_curve
+
+#%%
+config_path = os.path.realpath(sys.argv[1])
+config = yaml.load(open(config_path, 'r'), Loader=yaml.FullLoader)
+
+dataset = {}
+for suffix in sys.argv[2:]:
+    skiprows = 0
+    name = suffix.upper()
+    if suffix.lower() == 'snail':
+        skiprows = 1
+    if suffix.lower() == 'deseq':
+        name = 'DESeq'
+    if suffix.lower() == 'qsmooth':
+        name = 'Qsmooth'
+    if suffix.lower() == 'validation':
+        name = 'Validation'
+    
+    dataset[name] = pd.read_csv(
+        os.path.join(config['datasets_dir'], 'ENCODE', f'xprs_{suffix}.tsv'),
+        sep='\t', index_col=0, skiprows=skiprows
+    )
+
+tissues = pd.read_csv(
+    os.path.join(config['datasets_dir'], 'ENCODE', f'meta_tissue.tsv'),
+    sep='\\t', index_col=0, header=None
+)
+tissues = pd.Series(tissues.values.reshape(-1), index=tissues.index)
+tissue_exclusive_genes, tissue_exclusive_count = extract_tissue_exclusive_genes(
+    config['out_dir'],
+    'ENCODE',
+    'comparison',
+    dataset['Validation'],
+    tissues
+)
+all_tissue_exclusive_genes = list(it.chain.from_iterable(tissue_exclusive_genes.values()))
+tissue_of_gene, sample_order = sort_xprs_samples(
+    dataset['Validation'],
+    tissues,
+    tissue_exclusive_genes
+)
+
+#%%
+val = dataset['Validation']
+num_samples = val.shape[1]
+
+for threshold in [-1, 0, 25, 50, 75]:
+    if threshold == -1:
+        targets = val.loc[val.sum(axis=1) != 0].index
+        random_index = all_tissue_exclusive_genes
+    else:
+        criteria = ((val > 10).sum(axis=1) >= int(val.shape[1] * threshold / 100)) & ((val > 10).sum(axis=1) < int(val.shape[1] * (threshold + 25) / 100))
+        random_index = np.random.choice(
+            val.loc[criteria].index, 1000, replace=False)
+        random_index = np.random.choice(random_index, 1000, replace=False)
+
+    corr = []
+    names = []
+    for name, table in dataset.items():
+        if name != 'Validation':
+            corr.append(compute_correlation_coefficients(table.loc[random_index], 'spearman'))
+            names.append(name)
+
+    corr.insert(0, compute_correlation_coefficients(val.loc[random_index], 'spearman'))
+    names.insert(0, 'Validation')
+
+    if threshold == -1:
+        file_label = 'Tissue-exclusive genes'
+    else:
+        file_label = f'Expressed proportion [{threshold}%, {threshold+25}%)'
+
+    for metric in ['roc', 'prc']:
+        _ = bokeh_area_under_curve(
+            config['out_dir'],
+            file_label,
+            corr,
+            names,
+            ['green', 'chocolate', 'navy', 'green', 'blueviolet', 'crimson'],
+            metric,
+            False
+        )
---
+++ scripts/normalize_expression.R
@@ -0,0 +1,168 @@
+library(NormExpression)
+library(preprocessCore)
+
+qstats <- function(object, group_factor, window = 0.05)
+  {
+  # Compute sample quantiles
+  Q = apply(object, 2, sort) 
+  
+  # Compute quantile reference
+  Qref = rowMeans(Q)  
+  
+  # Compute SST
+  SST = rowSums((Q - Qref)^2)
+  
+  # Compute SSB
+  if(is.factor(group_factor)){
+    X = model.matrix(~ 0 + group_factor)
+  } else {
+    X = model.matrix(~ group_factor)
+  }
+  
+  QBETAS = t(solve(t(X) %*% X) %*% t(X) %*% t(Q))
+  Qhat = QBETAS %*% t(X)
+  SSB = rowSums((Qhat - Qref)^2)
+  
+  # Compute weights
+  roughWeights = 1 - SSB / SST
+  roughWeights[SST < 1e-6] = 1
+  
+  # Compute smooth weights
+  k = floor(window * nrow(Q))
+  if (k %% 2 == 0) k = k + 1
+  smoothWeights = runmed(roughWeights, k = k, endrule="constant")
+  
+  list(Q=Q, Qref=Qref, Qhat=Qhat, SST=SST, SSB=SSB, SSE=SST-SSB, 
+       roughWeights=roughWeights, smoothWeights=smoothWeights)
+}
+
+qsmooth <- function(object, group_factor,
+                    batch = NULL, norm_factors = NULL, 
+                    window = 0.05)
+{
+
+    object <- as.matrix(object)
+
+    if(ncol(object) != length(group_factor)){
+        stop("Number of columns in object does not match length of group_factor.")
+    } 
+
+    # Scale normalization step
+    if(!is.null(norm_factors)) { 
+        object <- sweep(object, 2, norm_factors, FUN = "/") 
+    }
+
+    # Compute quantile statistics
+    qs <- qstats(object=object, group_factor=group_factor, window=window)
+    Qref <- qs$Qref 
+    Qhat <- qs$Qhat  
+    w <- qs$smoothWeights
+    
+    # Weighted quantiles
+    objectNorm = w * Qref + (1 - w) * Qhat
+
+    # Re-order objectNorm by rank of object (columnwise)
+    for (i in seq_len(ncol(objectNorm))) {
+        # Grab ref. i
+        ref = objectNorm[,i]
+    
+        # Grab object column i
+        x = object[,i]
+    
+        # Grab ranks of x (using min rank for ties)
+        rmin = rank(x, ties.method="min")
+    
+        # If x has rank ties then average the values of ref at 
+        # those ranks
+        dups = duplicated(rmin)
+    
+        if (any(dups)) {
+            # Grab ranks of x (using random ranks for ties) 
+            # (needed to uniquely identify the indices of tied ranks)
+            rrand = rank(x, ties.method="random")
+            # Grab tied ranks
+            tied.ranks = unique(rmin[dups])
+            for (k in tied.ranks) {
+                # Select the indices of tied ranks 
+                sel = rrand[rmin == k] 
+                ref[sel] = ave(ref[sel])
+            }
+        }
+    
+    # Re-order ref and replace in objectNorm
+    objectNorm[,i] = ref[rmin]
+    }
+
+    rownames(objectNorm) = rownames(object)
+    colnames(objectNorm) = colnames(object)
+    return(objectNorm)
+}
+
+args <- commandArgs(trailingOnly=TRUE) 
+count <- args[1]
+group <- args[2]
+
+prefix <- substring(count, 1, nchar(count) - 9)
+count <- read.table(count, sep='\t', header=TRUE, stringsAsFactors=FALSE, row.names=1, check.names=FALSE)
+group  <- read.table(group, sep='\t', header=FALSE, stringsAsFactors=FALSE)
+rownames(group) <- group[, 1]
+group <- group[colnames(count), ]
+
+# DESeq
+filename <- paste0(prefix, 'deseq.tsv')
+sf <- getFactors(count, method = "DESeq")
+norm <- t(t(count) * sf)
+rownames(norm) <- rownames(count)
+colnames(norm) <- colnames(count)
+write.table(
+    norm, 
+    filename,
+    sep='\t', quote=FALSE, row.names=TRUE, col.names=NA
+)
+
+# UQ
+filename <- paste0(prefix, 'uq.tsv')
+sf <- getFactors(count, method = "UQ")
+norm <- t(t(count) * sf)
+rownames(norm) <- rownames(count)
+colnames(norm) <- colnames(count)
+write.table(
+    norm, 
+    filename,
+    sep='\t', quote=FALSE, row.names=TRUE, col.names=NA
+)
+
+# TMM
+filename <- paste0(prefix, 'tmm.tsv')
+sf <- getFactors(count, method = "TMM")
+norm <- t(t(count) * sf)
+rownames(norm) <- rownames(count)
+colnames(norm) <- colnames(count)
+write.table(
+    norm, 
+    filename,
+    sep='\t', quote=FALSE, row.names=TRUE, col.names=NA
+)
+
+# Qsmooth
+filename <- paste0(prefix, 'qsmooth.tsv')
+norm <- qsmooth(count, group[, 2])
+#norm <- qsmoothData(qsmooth_model)
+rownames(norm) <- rownames(count)
+colnames(norm) <- colnames(count)
+write.table(
+    norm, 
+    filename,
+    sep='\t', quote=FALSE, row.names=TRUE, col.names=NA
+)
+
+# Quantile
+filename <- paste0(prefix, 'quantile.tsv')
+norm <- normalize.quantiles(as.matrix(count))
+rownames(norm) <- rownames(count)
+colnames(norm) <- colnames(count)
+write.table(
+    norm, 
+    filename,
+    sep='\t', quote=FALSE, row.names=TRUE, col.names=NA
+)
